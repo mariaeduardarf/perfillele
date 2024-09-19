44ec6e2be1d519bf2985593753b4655bb0432c4e
Maria Eduarda <maria.rf.veleiros@gmail.com>
2024-09-19 15:48:02 -0300

    update button

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
 const toggleTheme = document.getElementById("toggleTheme");
-
 const rootHtml = document.documentElement;
 
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+//const menuLinks = document.querySelectorAll(".menu-link");
 
-//função para alternar o tema
-function changeTheme(){
+
+
+
+//função para alternar o tema 
+function changeTheme () {
     const currentTheme = rootHtml.getAttribute("data-theme");
 
-    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", 
-    "light") : rootHtml.setAttribute("data-theme", "dark");
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
 
     toggleTheme.classList.toggle("bi-sun");
     toggleTheme.classList.toggle("bi-moon-stars");
 
-}
+} 
 
 toggleTheme.addEventListener("click", changeTheme);
 //fim da função mudar o tema
 
 
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
 
-
-
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
